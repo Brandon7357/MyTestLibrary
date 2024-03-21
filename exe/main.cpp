79aa4cab3495947f4fee69b00575cedd8ede4ae5
Brandon7357 <theshadowlands@outlook.com>
2024-03-20 21:13:09 -0400

    Update main.cpp

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,26 +1,11 @@
 #include <iostream>
-#include <dlfcn.h>
-
-//#include "../lib/engineMain.h"
+#include "../include/myLibHeader.h"
 
 using namespace std;
 
 int main( int argc, char* argv[] ) {
 
-    void* handle;
-    handle = dlopen( "./libtestLib.so", RTLD_LAZY );
-
-    if( !handle ) {
-
-        cout<<"Cannot load engine library!"<<endl;
-        return 0;
-
-    };
-
-    typedef void (*engineThingy_t)();
-    engineThingy_t engineThingy = (engineThingy_t) dlsym( handle, "cmdRunMainEngineThingy" );
-    engineThingy();
-    dlclose( handle );
+    cmdRunMainEngineThingy();
     return 0;
 
 };
